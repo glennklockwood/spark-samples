852f1850b2e0c4a38a23b73d1ededed5942f3c69
Glenn K. Lockwood <glock@sdsc.edu>
2014-07-03 15:57:52 -0700

    added basic wordcount example

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-sparktutorial
+Spark Tutorial
 =============
 
-Spark Tutorial
+This represents the beginning of a tutorial I was creating for Spark users on 
+the Gordon supercomputer at the San Diego Supercomputer Center.  I ran out of 
+time when building out the full set of training material, but this is what I 
+thought might be useful and worth keeping for posterity.
---
+++ wordcount-spark.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env pyspark
+
+from pyspark import SparkContext
+
+### We need to figure out our Spark master and HDFS namenode
+import os
+spark_master_uri = "spark://%s:%s" % (  os.environ['SPARK_MASTER_IP'], 
+                                        os.environ['SPARK_MASTER_PORT'] )
+
+### Here we assume that the HDFS namenode is running on the same host
+### as our Spark master
+my_hdfs_dir =  "hdfs://%s:%d/user/%s/" % ( os.environ['SPARK_MASTER_IP'], 
+                                           54310,
+                                           os.environ['USER'] )
+
+### If you want to try this example using the interactive pyspark shell, 
+### sc will already exist based on your environemtn and the following
+### call to SparkContext() is not necessary
+sc = SparkContext( spark_master_uri, "Wordcount Test")
+
+### Do an actual word count ####################################################
+### Create an RDD from a file residing on HDFS
+file = sc.textFile(my_hdfs_dir + 'gutenberg.txt')
+
+### flatMap() can transform one element into zero or multiple elements
+words = file.flatMap(lambda line: line.split())
+
+### map() transforms every RDD element into a single new element
+keyvals = words.map(lambda word: (word, 1))
+
+### Given an RDD containing (key, value) tuples, return an RDD containing 
+### unique keys and a single reduced value for each
+counts = keyvals.reduceByKey(lambda a, b: a + b)
+
+### Save our RDD as a text file back to HDFS
+counts.saveAsTextFile(my_hdfs_dir + 'output.dir')
